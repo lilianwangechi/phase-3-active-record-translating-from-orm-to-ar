aba171942a7cb6c5c00158eb28939e5660aeef9b
lilian <lilian.wangechi@student.moringaschool.com>
2022-10-30 04:11:34 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,10 @@
-class Dog
+require "active_record"
+
+# Setup a database connection
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/dogs.sqlite"
+)
+
+class Dog < ActiveRecord::Base
 end
